ba0ae481f40533df2ee4e042679ac5db4488ec1e
Jeremie Jasmin <jeremie.jasmin@polymtl.ca>
2018-10-31 12:11:03 -0400

    JJ- button interraction and visual for the basic burrow

---
+++ Assets/Floater.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floater : MonoBehaviour {
+
+    public float floatingHeight = 0.2f;
+
+    public float floatingSpeed = 0.5f;
+
+    private float intitialHeight;
+
+    private float timer = 0;
+
+	// Use this for initialization
+	void Start () {
+        intitialHeight = transform.position.y;
+        timer = 0;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        timer += Time.deltaTime * floatingSpeed;
+        transform.position = new Vector3(transform.position.x, floatingHeight * Mathf.Sin(timer) + intitialHeight, transform.position.z);
+    }
+}
--- Assets/Scripts/Burrow.cs
+++ Assets/Scripts/Burrow.cs
@@ -11,11 +11,33 @@ public class Burrow : MonoBehaviour {
     // Whether the burrow is locked or not
     [SerializeField]
     private bool locked = false;
-    
-	void Update () {
-		// TODO Update the material when (un)locked
 
-	}
+
+    [SerializeField]
+    private MeshRenderer arrow;                 // the arrow floating above the burrow
+    [SerializeField]
+    private Material arrowShining;              // Material for when the arrow is visible
+    [SerializeField]
+    private Material arrowFaded;                // Material for when the arrow is transparent
+
+    private bool teleportationEnabled = false;  // If the player is on the burrow
+    private GameObject playerObj;
+
+    void Start()
+    {
+        FadeArrow(true);
+    }
+
+    void Update() {
+        // TODO Update the material when (un)locked
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (teleportationEnabled)
+            {
+                TeleportPlayer(playerObj);
+            }
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -24,7 +46,21 @@ public class Burrow : MonoBehaviour {
         GameObject go = other.gameObject;
         if (!locked && go.tag == "Player")
         {
-            TeleportPlayer(go);
+            playerObj = go;
+            FadeArrow(false);
+            teleportationEnabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // TODO fix problem with wall checks where wall checks trigger this instead
+        // For now the wall checks are disabled in the scene
+        GameObject go = other.gameObject;
+        if (!locked && go.tag == "Player")
+        {
+            FadeArrow(true);
+            teleportationEnabled = false;
         }
     }
 
@@ -33,13 +69,29 @@ public class Burrow : MonoBehaviour {
         // Check if the player entered from the right or left
         Transform playerTransform = player.transform;
         Vector3 endSize = end.GetComponent<Collider>().bounds.size;
-        float xOffset = playerTransform.position.x < transform.position.x ? endSize.x / 2 + 0.5f : -(endSize.x / 2 + 0.5f);
+        //float xOffset = playerTransform.position.x < transform.position.x ? endSize.x / 2 + 0.5f : -(endSize.x / 2 + 0.5f);
 
         // Teleport the player to the other burrow on the opposite side he entered from
         float playerHeight = player.GetComponent<Collider>().bounds.size.y;
         Vector3 otherEndPos = end.transform.position;
-        float y = otherEndPos.y - endSize.y / 2 + playerHeight / 2;
-        playerTransform.position = new Vector3(otherEndPos.x + xOffset, y, otherEndPos.z);
+        float y = otherEndPos.y - endSize.y / 2 + playerHeight / 2 + 0.5f;
+        playerTransform.position = new Vector3(otherEndPos.x, y, otherEndPos.z);
+    }
+
+    private void FadeArrow(bool shouldFade)
+    {
+        
+        if (arrow)
+        {
+            if (shouldFade)
+            {
+                arrow.material = arrowFaded;
+            }
+            else
+            {
+                arrow.material = arrowShining;
+            }
+        }
     }
 
     public void Lock()
