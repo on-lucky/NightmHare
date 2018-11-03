210624c9673be223fde1945a14f5d14f8083127b
Fabrice Dugas <fabby.dugas@gmail.com>
2018-11-02 23:43:47 -0400

    Add sinus effect for coolness

--- Assets/FloatingKey.cs
+++ Assets/FloatingKey.cs
@@ -20,16 +20,15 @@ public class FloatingKey : MonoBehaviour {
 
     private Vector3 destination;
 
-    private float distance;
-
     [SerializeField]
     private float speed = 3;
 
     [SerializeField]
     private float curveHeight = 2;
 
-    // Quadratic equation y = ax^2 + bx + c
-    private float a, b, c;
+    private QuadraticCurve curve;
+
+    private bool recalculateCurve = true;
 
     void Start()
     {
@@ -39,18 +38,26 @@ public class FloatingKey : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update () {
-		if (go)
+    void Update() {
+        if (go)
         {
+            if (recalculateCurve)
+            {
+                GetCurve2D();
+                recalculateCurve = false;
+            }
+
             Vector3 pos = transform.position;
             if (Vector3.Distance(pos, destination) <= 0.5) {
                 transform.position = destination;
             } else
             {
-                float dx = Time.deltaTime * speed;
-                float nx = pos.x + dx;
-                float ny = a*Mathf.Pow(nx, 2) + b*nx + c;
-                transform.position = new Vector3(nx, ny, pos.z);
+                // Quadratic curve for the trajectory
+                // Sinus wave for extra coolness
+                float x = pos.x + Time.deltaTime * speed;
+                float ox = x - origin.x;
+                float y = curve.compute(x) + Mathf.Sin(x);
+                transform.position = new Vector3(x, y, pos.z);
             }
         }
 
@@ -58,8 +65,9 @@ public class FloatingKey : MonoBehaviour {
         else
         {
             transform.position = origin;
+            recalculateCurve = true;
         }
-	}
+    }
 
     // Starts the movement
     public void Go(Vector3 destination)
@@ -71,32 +79,34 @@ public class FloatingKey : MonoBehaviour {
 
     private void GetCurve2D()
     {
-        // Solve the quadratic curve that passes
-        // through both the origin and the destination
+        Vector3 mid = new Vector3(
+                    (origin.x + destination.x) / 2,
+                    Mathf.Max(origin.y, destination.y) + curveHeight,
+                    (origin.z + destination.z) / 2);
+        curve = new QuadraticCurve(origin, mid, destination);
+    }
 
-        // Solve with three points: origin, destination and
-        // a point in between higher than the highest point
-        Vector3 p1 = origin;
-        Vector3 p2 = destination;
-        Vector3 p3 = new Vector3(
-            (p1.x + p2.x)/2,
-            Mathf.Max(p1.y, p2.y) + curveHeight,
-            (p1.z + p2.z) / 2
-            );
+    class QuadraticCurve
+    {
+        // y = ax^2 + bx + c
+        float a, b, c;
 
-        // Calculate A, B, and C.
-        /*
-        A = [(Y2-Y1)(X1-X3) + (Y3-Y1)(X2-X1)]/[(X1-X3)(X2^2-X1^2) + (X2-X1)(X3^2-X1^2)]
+        public QuadraticCurve(Vector3 p1, Vector3 p2, Vector3 p3)
+        {
+            /* Calculate a, b and c
+             * A = [(Y2-Y1)(X1-X3) + (Y3-Y1)(X2-X1)]/[(X1-X3)(X2^2-X1^2) + (X2-X1)(X3^2-X1^2)]
+             * B = [(Y2 - Y1) - A(X2^2 - X1^2)] / (X2-X1)
+             * C = Y1 - AX1^2 - BX1
+            */
+            a = ((p2.y - p1.y) * (p1.x - p3.x) + (p3.y - p1.y) * (p2.x - p1.x)) /
+                ((p1.x - p3.x) * (p2.x * p2.x - p1.x * p1.x) + (p2.x - p1.x) * (p3.x * p3.x - p1.x * p1.x));
+            b = ((p2.y - p1.y) - a * (p2.x * p2.x - p1.x * p1.x)) / (p2.x - p1.x);
+            c = p1.y - a * p1.x * p1.x - b * p1.x;
+        }
 
-        B = [(Y2 - Y1) - A(X2^2 - X1^2)] / (X2-X1)
-
-        C = Y1 - AX1^2 - BX1
-        */
-
-        a = ((p2.y - p1.y) * (p1.x - p3.x) + (p3.y - p1.y) * (p2.x - p1.x)) /
-            ((p1.x - p3.x) * (p2.x * p2.x - p1.x * p1.x) + (p2.x - p1.x) * (p3.x * p3.x - p1.x * p1.x));
-        b = ((p2.y - p1.y) - a * (p2.x * p2.x - p1.x * p1.x)) / (p2.x - p1.x);
-        c = p1.y - a * p1.x * p1.x - b * p1.x;
-
+        public float compute(float x)
+        {
+            return a * Mathf.Pow(x, 2) + b * x + c;
+        }
     }
 }
