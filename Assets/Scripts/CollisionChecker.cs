c251b54bc18a06eda3dcd5285e274f7aebf9e544
Jérémie Jasmin <jeremiejasmin1@hotmail.com>
2018-11-06 17:49:47 -0500

    JJ- revamp of Gym_5 to Gym_10

--- Assets/Scripts/CollisionChecker.cs
+++ Assets/Scripts/CollisionChecker.cs
@@ -58,7 +58,6 @@ public class CollisionChecker : MonoBehaviour {
             case CheckerType.Ground:
                 if (obj.tag == "Ground")
                 {
-                    Debug.Log("fall");
                     messageReceiver.GetComponent<Jumper>().Fall();
                 }
                 break;
