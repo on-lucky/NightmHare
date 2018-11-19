bdbc1037d625f9a8d269eb940c8c59e3e8e64ec9
Fabrice Dugas <faberice.dugas@gmail.com>
2018-11-19 13:31:47 -0500

    Add carrot and rotation to floater

--- Assets/Scripts/Floater.cs
+++ Assets/Scripts/Floater.cs
@@ -8,6 +8,8 @@ public class Floater : MonoBehaviour {
 
     public float floatingSpeed = 0.5f;
 
+    public float rotationSpeed = 180;
+
     private float intitialHeight;
 
     private float timer = 0;
@@ -22,5 +24,7 @@ public class Floater : MonoBehaviour {
 	void FixedUpdate () {
         timer += Time.deltaTime * floatingSpeed;
         transform.position = new Vector3(transform.position.x, floatingHeight * Mathf.Sin(timer) + intitialHeight, transform.position.z);
+        Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z + rotationSpeed);
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime, Space.World);
     }
 }
