36f86678b7504b4d9b29b9e89498b983e4efb3cd
Fabrice Dugas <faberice.dugas@gmail.com>
2018-11-19 14:33:20 -0500

    Add carrot mechanic

---
+++ Assets/CarrotCollector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CarrotCollector : MonoBehaviour
+{
+    [SerializeField]
+    private int carrotCount = 0;
+
+    [SerializeField]
+    private Text carrotCountText;
+
+    public void Collect(Carrot carrot)
+    {
+        carrotCount++;
+        UpdateHUD();
+    }
+
+    private void UpdateHUD()
+    {
+        if (carrotCountText) carrotCountText.text = carrotCount.ToString();
+    }
+}
