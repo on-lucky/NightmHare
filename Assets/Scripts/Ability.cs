262aac5145ee2471b0e31a8bddd425fa69a416ba
Fabrice Dugas <faberice.dugas@gmail.com>
2018-11-23 12:44:27 -0500

    Add Cooldown to HUD

---
+++ Assets/Scripts/Ability.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ability : MonoBehaviour {
+
+    private float cooldown;
+    private bool isCooldown = false;
+    [SerializeField] private Image imageCooldown;
+	
+	// Update is called once per frame
+	void Update () {
+		if (isCooldown)
+        {
+            imageCooldown.fillAmount -= 1 / cooldown * Time.deltaTime;
+
+            if (imageCooldown.fillAmount <= 0)
+            {
+                imageCooldown.fillAmount = 0;
+                isCooldown = false;
+            }
+        }
+	}
+
+    public void StartCooldown(float cooldown)
+    {
+        Fill();
+        this.cooldown = cooldown;
+        isCooldown = true;
+    }
+
+    public void Fill()
+    {
+        imageCooldown.fillAmount = 1;
+    }
+}
