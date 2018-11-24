e23247e9e20904c9eaefce44e3df188b6cd0dd3b
Fabrice Dugas <fabby.dugas@gmail.com>
2018-11-24 13:38:24 -0500

    Finish CameraTransform

--- Assets/CameraTransform.cs
+++ Assets/CameraTransform.cs
@@ -7,9 +7,6 @@ public class CameraTransform : MonoBehaviour {
     // How long to do the transformations
     [SerializeField] private float duration = 1;
 
-    // Indicates if forward is to the right
-    [SerializeField] private bool isForward = true;
-
     // Transformations to do on the camera
     [SerializeField] private float offsetY = 0;
     [SerializeField] private float z = 0;
@@ -20,7 +17,6 @@ public class CameraTransform : MonoBehaviour {
     private float zSpeed = 1;
     private float rotSpeed = 1;
     private float currentTime = 0;
-    private float forward = 1;
 
     // Original values
     private float originOffsetY = 0;
@@ -34,7 +30,7 @@ public class CameraTransform : MonoBehaviour {
 
     private void Start()
     {
-        // Save current values
+        // Save original values
         originOffsetY = CameraFollower.instance.GetYOffset();
         originZ = Camera.main.transform.position.z;
         originRotation = Camera.main.transform.rotation.eulerAngles.x;
@@ -52,10 +48,10 @@ public class CameraTransform : MonoBehaviour {
             {
                 go = false;
 
-                // Clip final values to what they should be
+                // Clip final values to what they should really be to avoid accumulating floating point errors
                 var camTransform = CameraFollower.instance.transform;
+                camTransform.position = new Vector3(camTransform.position.x, camTransform.position.y, finalZ);
                 CameraFollower.instance.SetYOffset(finalOffsetY);
-                camTransform.position = new Vector3(camTransform.position.x, finalOffsetY, finalZ);
                 camTransform.rotation = Quaternion.AngleAxis(finalRotation, Vector3.right);
             }
         }
@@ -67,11 +63,14 @@ public class CameraTransform : MonoBehaviour {
         if (other.tag == "Player")
         {
             // Calculate speeds
+            var yOffset = CameraFollower.instance.GetYOffset();
             var camPos = Camera.main.transform.position;
-            var camRotation = Camera.main.transform.rotation.eulerAngles;
-            ySpeed = (offsetY - camPos.y) / duration;
+            ySpeed = (offsetY - yOffset) / duration;
             zSpeed = (z - camPos.z) / duration;
-            rotSpeed = (rotation - camRotation.x) / duration;
+
+            // Calculate rotation speed
+            var camRotation = Camera.main.transform.rotation.eulerAngles;
+            rotSpeed = Mathf.DeltaAngle(camRotation.x, rotation) / duration;
 
             // Set final values
             finalOffsetY = offsetY;
@@ -88,14 +87,15 @@ public class CameraTransform : MonoBehaviour {
     {
         if (other.tag == "Player")
         {
-            // Return to original values
-
-            // Calculate speeds
+            // Calculate speeds to restore original values
+            var yOffset = CameraFollower.instance.GetYOffset();
             var camPos = Camera.main.transform.position;
-            var camRotation = Camera.main.transform.rotation.eulerAngles;
-            ySpeed = (originOffsetY - camPos.y) / duration;
+            ySpeed = (originOffsetY - yOffset) / duration;
             zSpeed = (originZ - camPos.z) / duration;
-            rotSpeed = (originRotation - camRotation.x) / duration;
+
+            // Calculate rotation speed
+            var camRotation = Camera.main.transform.rotation.eulerAngles;
+            rotSpeed = Mathf.DeltaAngle(camRotation.x, originRotation) / duration;
 
             // Set final values
             finalOffsetY = originOffsetY;
@@ -110,18 +110,23 @@ public class CameraTransform : MonoBehaviour {
 
     private void UpdateTranslation()
     {
-        var deltaY = forward * ySpeed * Time.deltaTime;
-        var deltaZ = forward * zSpeed * Time.deltaTime;
-        var pos = CameraFollower.instance.transform.position;
-        CameraFollower.instance.SetYOffset(pos.y + deltaY);
-        CameraFollower.instance.transform.position = new Vector3(pos.x, pos.y + deltaY, pos.z + deltaZ);
+        var deltaY = ySpeed * Time.deltaTime;
+        var deltaZ = zSpeed * Time.deltaTime;
+        var camPos = CameraFollower.instance.transform.position;
+
+        // Apply y offset
+        var yOffset = CameraFollower.instance.GetYOffset();
+        CameraFollower.instance.SetYOffset(yOffset + deltaY);
+
+        // Apply z translation
+        CameraFollower.instance.transform.position = new Vector3(camPos.x, camPos.y, camPos.z + deltaZ);
     }
 
     private void UpdateRotation()
     {
-        var deltaRot = forward * rotSpeed * Time.deltaTime;
+        var deltaRot = rotSpeed * Time.deltaTime;
         var camTransform = CameraFollower.instance.transform;
-        var eulers = camTransform.rotation.eulerAngles + new Vector3(deltaRot, 0, 0);
-        camTransform.rotation = Quaternion.Euler(eulers);
+        var xAngle = camTransform.rotation.eulerAngles.x;
+        camTransform.rotation = Quaternion.AngleAxis(xAngle+deltaRot, Vector3.right);
     }
 }
