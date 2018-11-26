721bfffa489bf7be918548249069e1a177d413d1
Jeremie Jasmin <jeremie.jasmin@polymtl.ca>
2018-11-26 11:36:25 -0500

    JJ - loadpoint on main scene

---
+++ Assets/LoadPoint.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadPoint : MonoBehaviour {
+
+    public GameObject[] loadables;
+    public GameObject[] unloadables;
+
+    void OnTriggerEnter(Collider other)
+    {
+        GameObject obj = other.gameObject;
+
+        if (obj.tag == "Player")
+        {
+            foreach(GameObject o in loadables)
+            {
+                o.SetActive(true);
+            }
+
+            foreach (GameObject o in unloadables)
+            {
+                o.SetActive(false);
+            }
+        }
+    }
+}
